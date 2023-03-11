ed4c15ed13f2b17e3c84cc1039e199550983c5a0
nicktudan <107770913+nicktudan@users.noreply.github.com>
2023-03-11 14:06:08 +0100

    Refactors Modal into hooks

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,45 +1,77 @@
 import PropTypes from 'prop-types';
-import { Component } from 'react';
+import { useEffect } from 'react';
 import { createPortal } from "react-dom";
 import { Overlay, ModalWindow } from './Modal.styled';
 
 const modalRoot = document.querySelector('#modal-root');
 
 
-export class Modal extends Component {
+export const Modal = ({ onClose, children }) => {
+
+  useEffect(() => {
+    const handleKeyDown = evt => {
+      if (evt.code === 'Escape') {
+        onClose();
+      }
+    };
     
-    componentDidMount () {
-        window.addEventListener('keydown', this.handleKeyDown)
-    }
-
-    componentWillUnmount () {
-        window.removeEventListener('keydown', this.handleKeyDown)
-    }
-
-    handleKeyDown = evt => {
-        if(evt.code === 'Escape') {
-            this.props.onClose();
-        }
-    }
-
-    handleOverlayClick = evt => {
-        if(evt.currentTarget === evt.target) {
-            this.props.onClose();
-        }
-    }
+      window.addEventListener('keydown', handleKeyDown);
+      return () => {
+        window.removeEventListener('keydown', handleKeyDown);
+      };
+  }, [onClose]);
 
 
-    render() {
-        return createPortal(
-            <Overlay onClick={this.handleOverlayClick}>
-                <ModalWindow>
-                    {this.props.children}
-                </ModalWindow>
-            </Overlay>,
-            modalRoot,
-        )
+  const handleOverlayClick = evt => {
+    if (evt.currentTarget === evt.target) {
+      onClose();
     }
+  };
+
+
+  return createPortal(
+    <Overlay onClick={handleOverlayClick}>
+      <ModalWindow>{children}</ModalWindow>
+    </Overlay>,
+    modalRoot
+  );
 }
+
+
+// export class Modal extends Component {
+    
+//     componentDidMount () {
+//         window.addEventListener('keydown', this.handleKeyDown)
+//     }
+
+//     componentWillUnmount () {
+//         window.removeEventListener('keydown', this.handleKeyDown)
+//     }
+
+//     handleKeyDown = evt => {
+//         if(evt.code === 'Escape') {
+//             this.props.onClose();
+//         }
+//     }
+
+//     handleOverlayClick = evt => {
+//         if(evt.currentTarget === evt.target) {
+//             this.props.onClose();
+//         }
+//     }
+
+
+//     render() {
+//         return createPortal(
+//             <Overlay onClick={this.handleOverlayClick}>
+//                 <ModalWindow>
+//                     {this.props.children}
+//                 </ModalWindow>
+//             </Overlay>,
+//             modalRoot,
+//         )
+//     }
+// }
 
 Modal.propTypes = {
     onClose: PropTypes.func.isRequired,
